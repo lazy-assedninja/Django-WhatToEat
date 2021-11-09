29db73ec5c5ba4c4bff63689d7c4a795b48a8976
Lazy-assed Ninja <henryhuang861219@gmail.com>
2021-11-09 18:02:39 +0800

    update: change sqlite to mysql

--- core/settings.py
+++ core/settings.py
@@ -67,7 +67,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv("DB_ENGINE"),
-        'NAME': BASE_DIR / os.getenv("DB_NAME"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
